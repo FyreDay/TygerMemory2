fcb054c0af0b0d00c8e2197b366ea8591ad323af
FyreDay <fyredaymc@gmail.com>
2025-05-14 19:19:51 -0500

    save fix

--- TygerMemory2/SaveData.cpp
+++ TygerMemory2/SaveData.cpp
@@ -6,7 +6,7 @@ SaveDataStruct* SaveData::Data = nullptr;
 SaveDataStruct* SaveData::GetData() {
     if (!Data) { // Initialize only if Data hasn't been set
         uintptr_t address = Core::moduleBase + 0x4EB580;
-        Data = reinterpret_cast<SaveDataStruct*>(*reinterpret_cast<int*>(address));
+        Data = reinterpret_cast<SaveDataStruct*>(reinterpret_cast<int*>(address));
     }
     return Data;
 }
