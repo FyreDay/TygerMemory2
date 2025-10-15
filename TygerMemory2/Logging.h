907925922e43075c7a5aaf3b6ed9b000b10115c3
FyreDay <fyredaymc@gmail.com>
2025-10-14 19:35:21 -0500

    fix logging extern

--- TygerMemory2/Logging.h
+++ TygerMemory2/Logging.h
@@ -1,5 +1,9 @@
 #pragma once
-
+#ifdef TYGERMEM2_EXPORTS
+#define TYGERMEM2 __declspec(dllexport)
+#else
+#define TYGERMEM2 __declspec(dllimport)
+#endif
 #include <string>
 #include <fstream>
 #include "framework.h"
